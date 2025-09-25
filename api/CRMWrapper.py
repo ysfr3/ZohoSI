343e603b6839ee534eaab20f63c169ca78c77176
Aidan Lelliott <aidan.lelliott@asw.local>
2025-09-25 15:50:24 -0400

    main

--- api/CRMWrapper.py
+++ api/CRMWrapper.py
@@ -1,14 +1,39 @@
+import os
 import requests
+from dotenv import load_dotenv
 
 class CRMWrapper:
-    def __init__(self, token: str, base_url: str = 'https://www.zohoapis.com/crm/v8/Deals/'):
+    def __init__(self, base_url: str = 'https://www.zohoapis.com/crm/v8/Deals/'):
         self.url = base_url if base_url.endswith('/') else f'{base_url}/'
-        self.token = token
         self._headers = {
-            "Authorization": f"Zoho-oauthtoken {self.token}",
+            "Authorization": f"Zoho-oauthtoken ",
             "Content-Type": "application/json"
         }
 
     def push_new_deal_data(self, dealId: int, data: dict) -> dict:
+        """
+        Push new data to existing deal in Zoho CRM
+
+        Args:
+            dealId (int): dealId of deal to update
+            data (dict): New data to push to deal
+
+        Returns:
+            dict: Response from Zoho CRM API
+        """
+        self._headers["Authorization"] = f"Zoho-oauthtoken {self._gen_crm_token()}"
         response = requests.post(f'{self.url}{dealId}', json=data, headers=self._headers)
         return response.json()
+    
+    def _gen_crm_token(self) -> str:
+        """
+        Get new CRM token from Zoho
+
+        Returns:
+            str: access token
+        """
+        load_dotenv()
+        base_url_access = f"https://accounts.zoho.com/oauth/v2/token?client_id={os.getenv("CRM_ID")}&client_secret={os.getenv("CRM_SECRET")}&grant_type=client_credentials&scope=ZohoCRM.modules.deals.ALL,ZohoCRM.modules.accounts.ALL,ZohoCRM.org.ALL&soid={os.getenv("CRM_SOID")}"
+        response = requests.post(base_url_access)
+
+        return response.json().get("access_token")
